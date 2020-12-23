aad273a2a82f44806b6da1ac190d0d31d7b01023
zizhuxianzong <775477940@qq.com>
2020-12-23 21:42:09 +0800

    初始化

---
+++ mybee/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+    div {
+      background: greenyellow;
+      width: 75px;
+      height: 75px;
+      font-size: 18PX;
+    }
+  }
+</style>
---
+++ mybee/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+   <div>你好</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+.home {
+    div {
+      background: greenyellow;
+      width: 75px;
+      height: 75px;
+      font-size: 18PX;
+    }
+  }
+</style>
